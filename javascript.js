fe7ea7fb6ef7e8c6544017b166a41a33651b48f5
nocpinmylf <lemduc@hotmail.com>
2020-04-13 15:23:16 +0700

    init repo

---
+++ javascript.js
@@ -0,0 +1,81 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("demo");
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" slide-active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " slide-active";
+}
+
+// search
+const searchValue = document.getElementById('search-box');
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', () => {
+  searchBtn.setAttribute('href', `https://www.google.nl/search?q=${searchValue.value}+huc.edu.vn`);
+});
+
+// get time 
+const time = document.getElementById('time').innerText = getTime();
+
+function getTime() {
+  let date = new Date();
+  return "Ngày: " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+}
+
+// vertical nav dropdown
+$('.sub-menu').hide();
+$('.menu-item>a').click(function () {
+  $(this).parent('.menu-item').children('ul').slideToggle('100');
+  $(this).find('.right').toggleClass('fa-caret-up fa-caret-down');
+});
+
+// responsive header navbar
+let dWidth;
+
+function resizeSlideBtn(width) {
+  $('.prev').css('top', width);
+  $('.next').css('top', width);
+}
+
+$(window).resize(function () {
+  dWidth = $(window).width();
+  if (dWidth > 1024) {
+    resizeSlideBtn('21em');
+    $('#header-nav li').removeAttr('style');
+  }
+  if (dWidth < 1024) {
+    resizeSlideBtn('22em');
+  }
+  if (dWidth < 768) {
+    resizeSlideBtn('15em');
+  }
+});
+
+$('#collaspe-btn').click(function () {
+  $('#header-nav li').slideToggle('100');
+  if ($('.prev').css('top') == '720px') {
+    resizeSlideBtn('15em');
+  }
+  else {
+    resizeSlideBtn('720px');
+  }
+});
+
